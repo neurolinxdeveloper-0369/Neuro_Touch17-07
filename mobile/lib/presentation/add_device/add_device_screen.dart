7b423376138f958b4e51ce520e72ec1c3bf47a08
Neurolinx Developer <developer@neurolinx.io>
2026-07-27 11:22:23 +0530

    chore: update gas knob ssid pattern to match hyphen format

--- mobile/lib/presentation/add_device/add_device_screen.dart
+++ mobile/lib/presentation/add_device/add_device_screen.dart
@@ -87,7 +87,7 @@ class _AddDeviceScreenState extends ConsumerState<AddDeviceScreen> {
     const DeviceModelDef(name: 'Touch Panel 8S', category: 'Custom IoT Device', icon: Icons.settings_input_component_rounded, deviceType: DeviceType.customTouchPanel, ssidPattern: 'Touch_Panel_8S', switchCount: 8),
 
     // Smart Kitchen
-    const DeviceModelDef(name: 'Gas Stove Controller', category: 'Smart Kitchen', icon: Icons.local_fire_department_rounded, deviceType: DeviceType.gasControl, ssidPattern: 'GasKnob_', switchCount: 0),
+    const DeviceModelDef(name: 'Gas Stove Controller', category: 'Smart Kitchen', icon: Icons.local_fire_department_rounded, deviceType: DeviceType.gasControl, ssidPattern: 'GasKnob-', switchCount: 0),
   ];
 
   @override
